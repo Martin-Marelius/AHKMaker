4b8e4c1209bc1021247a6abe6c727728278967f6
Martin-Marelius <johnsen.martin1@googlemail.com>
2022-04-10 20:04:21 +0200

    initial commit

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+
+/**
+ * @returns Footer section with links to important information
+ */
+
+const Footer = () => {
+    return (
+        <div className="flex flex-col w-screen bottom-12 inset-x-0 bg-inherit">
+            footer
+
+
+        </div>
+    );
+}
+
+export default Footer
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { Footer, Navbar } from '../components'
+
+
+const IndexPage = () => {
+  return (
+    <div className="bg-slate-200 h-screen">
+      Homescreen
+      <Footer />
+    </div>
+  )
+}
+
+export default IndexPage
+
+
